34b30b48f15eaa76bf233a1741233e2d161a4018
m8 <umusasadik@gmail.com>
2022-07-11 13:34:50 +0200

    Update makefiles

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Build leveldb
+if [ ! -f deps/leveldb/build ]; then
+    cd deps/leveldb
+    mkdir -p build && cd build
+    cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
+    cd ../../../
+fi
+
 make clean
 make -s
-sudo ./dp/shinjuku
+LD_PRELOAD=./deps/opnew/dest/libnew.so ./dp/shinjuku
