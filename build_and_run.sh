9e745498352b0bce249fb2010736d89cef740459
rishabh246 <rishabh246@gmail.com>
2022-10-20 00:46:53 +0200

    Remove leveldb build from shinjuku's build script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Build leveldb
-if [ ! -f ./deps/leveldb/build ]; then
-    cd deps/leveldb
-    mkdir -p build && cd build
-    cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build .
-    cd ../../../
-fi
-
 rm -rf /tmpfs/experiments/leveldb/
 make clean
 make -j6 -s
