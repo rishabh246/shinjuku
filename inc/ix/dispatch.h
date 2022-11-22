15a8e9eef34eb2fbbf1db450c90c837cb11e1eb3
rishabh246 <rishabh246@gmail.com>
2022-11-22 18:05:59 +0100

    Add missing return type

--- inc/ix/dispatch.h
+++ inc/ix/dispatch.h
@@ -54,11 +54,11 @@ struct mempool mcell_mempool __attribute((aligned(64)));
 #define JBSQ_LEN    0x02
 
 #if JBSQ_LEN == 0x02
-static inline jbsq_get_next(uint8_t iter){
+static inline uint8_t jbsq_get_next(uint8_t iter){
         return iter^1; // This is for JBSQ_LEN = 2
 }
 #elif JBSQ_LEN == 0x01
-static inline jbsq_get_next(uint8_t iter){
+static inline uint8_t jbsq_get_next(uint8_t iter){
         return iter;
 }
 #endif
