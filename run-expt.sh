045078c4f62c8e71f042fce008d5a21e57914963
rishabh246 <rishabh246@gmail.com>
2022-12-02 23:47:38 +0100

    Print rps in output file

--- run-expt.sh
+++ run-expt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PERCENTILE=99
-declare -a load_levels=("1" "10" "20" "30" "40" "50" "60" "70" "80" "90" "100" "110" "120")
+declare -a load_levels=("1" "10" "20" "30" "40" "50" "60" "70" "80" "90" "100")
 sudo rm -f latency.csv slowdown.csv temp.csv temp.txt latency.txt slowdown.txt 
 touch latency.csv slowdown.csv temp.txt
 for load in "${load_levels[@]}"
@@ -10,18 +10,17 @@ for load in "${load_levels[@]}"
     sudo ./build_and_run.sh $load > temp.txt
     sed -i '$ d' temp.txt 
     RPS=$(grep "Dispatched pkts" temp.txt | cut -d ":" -f6)
-    echo "Achieved rate is $RPS"
 
     echo "Calculating latency"
     grep "latency, slowdown" temp.txt | cut -d ":" -f5 > latency.txt
     python3 ../scripts/percentile.py latency.txt temp.csv
-    LATENCY="$load,$(grep "$PERCENTILE," temp.csv | cut -d "," -f2)"
+    LATENCY="$load,$RPS,$(grep "$PERCENTILE," temp.csv | cut -d "," -f2)"
     echo $LATENCY >> latency.csv
 
     echo "Calculating slowdown"
     grep "latency, slowdown" temp.txt | cut -d ":" -f6 > slowdown.txt 
     python3 ../scripts/percentile.py slowdown.txt temp.csv
-    LATENCY="$load,$(grep "$PERCENTILE," temp.csv | cut -d "," -f2)"
+    LATENCY="$load,$RPS,$(grep "$PERCENTILE," temp.csv | cut -d "," -f2)"
     echo $LATENCY >> slowdown.csv
   done
 
